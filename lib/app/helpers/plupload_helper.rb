49de16aee01f7b7e4a3479e6142d3e3313bc4538
Sammy Larbi <sammy@media3creative.com>
2011-05-27 14:01:37 -0500

    fixed option for file filter

--- lib/app/helpers/plupload_helper.rb
+++ lib/app/helpers/plupload_helper.rb
@@ -2,7 +2,7 @@ module PluploadHelper
   def plupload(model_object, model_object_method, options={})
     options[:plupload_container] ||= 'uploader'
     options[:max_file_size] ||= 10
-    options[:filters] ||= [{:title=>'All Files', :extensions=>'*'}]
+    options[:filters] ||= []
     render :partial=>'plupload/uploader_scripts', :locals=>{
         :model_object=>model_object, 
         :model_object_method=>model_object_method, 
