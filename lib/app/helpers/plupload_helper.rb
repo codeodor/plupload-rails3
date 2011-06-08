51c7a46815ad5db6ab4bfc6ac12e9e4a2bf83cc9
Sammy Larbi <sam@codeodor.com>
2011-06-08 06:13:26 -0500

    added default value for params to fix error reported in issue 1 by thenetduck

--- lib/app/helpers/plupload_helper.rb
+++ lib/app/helpers/plupload_helper.rb
@@ -3,6 +3,7 @@ module PluploadHelper
     options[:plupload_container] ||= 'uploader'
     options[:max_file_size] ||= 10
     options[:filters] ||= []
+    options[:params] ||= {}
     render :partial=>'plupload/uploader_scripts', :locals=>{
         :model_object=>model_object, 
         :model_object_method=>model_object_method, 
