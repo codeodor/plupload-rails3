e116933145ef96ef55138a43093a771889063299
Sammy Larbi <sam@codeodor.com>
2011-07-06 16:14:37 -0500

    changed install and uninstall to create and remove plupload-rails3 tmp dir

--- install.rb
+++ install.rb
@@ -2,3 +2,4 @@ require 'fileutils'
 FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/images/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/images'))
 FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/javascripts/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/javascripts'))
 FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/stylesheets/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/stylesheets'))
+FileUtils.mkdir(File.join(File.dirname(__FILE__),'../../../tmp/plupload-rails3'))
