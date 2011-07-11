d607df42aab5bcc99eeccb747ca595a72e488c6e
reaper <reaper@reaperville.com>
2011-07-11 17:15:04 +0900

    Modified to work with Rails 3.1rc4.

--- lib/app/middleware/plupload_params_renamer.rb
+++ lib/app/middleware/plupload_params_renamer.rb
@@ -1,18 +1,19 @@
-require 'rack/utils'
- 
-class PluploadParamsRenamer
-  def initialize(app)
-    @app = app
-  end
- 
-  def call(env)
-    req = Rack::Request.new(env)
-    if req.POST["_plupload_upload"]
-      object, method = req.params["_plupload_upload"].split(/[\[\]]/)
-      req.params[object] ||= {} 
-      req.params[object][method] = req.params["file"] #req.POST[req.POST["_plupload_upload"]] = req.POST["file"]
-    end 
-    
-    @app.call(env)
+require 'action_dispatch/http/request'
+module ActionDispatch
+  class PluploadParamsRenamer
+    def initialize(app)
+      @app = app
+    end
+   
+    def call(env)
+      req =  Request.new(env)
+      if req.POST["_plupload_upload"]
+        object, method = req.params["_plupload_upload"].split(/[\[\]]/)
+        req.params[object] ||= {} 
+        req.params[object][method] = req.params["file"] #req.POST[req.POST["_plupload_upload"]] = req.POST["file"]
+      end 
+      
+      @app.call(env)
+    end
   end
 end
