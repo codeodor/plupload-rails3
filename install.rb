96505c059ae3401925922e8403c78949430d04f2
Sammy Larbi <sam@codeodor.com>
2011-08-23 08:58:18 -0500

    changes to install/uninstall script to find the right directory better, and hopefully fix issue 9 with rails3.1

--- install.rb
+++ install.rb
@@ -1,5 +1,10 @@
 require 'fileutils'
-FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/images/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/images'))
-FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/javascripts/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/javascripts'))
-FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/stylesheets/plupload-rails3'), File.join(File.dirname(__FILE__), '../../../public/stylesheets'))
+
+image_path_prefix = '../../../public'
+js_path_prefix = '../../../public'
+css_path_prefix = '../../../public'
+
+FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/images/plupload-rails3'), File.join(File.dirname(__FILE__), "#{image_path_prefix}/images/plupload-rails3"))
+FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/javascripts/plupload-rails3'), File.join(File.dirname(__FILE__), "#{js_path_prefix}/javascripts/plupload-rails3"))
+FileUtils.cp_r(File.join(File.dirname(__FILE__), 'lib/public/stylesheets/plupload-rails3'), File.join(File.dirname(__FILE__), "#{css_path_prefix}/stylesheets/plupload-rails3"))
 FileUtils.mkdir(File.join(File.dirname(__FILE__),'../../../tmp/plupload-rails3'))
--- uninstall.rb
+++ uninstall.rb
@@ -1,5 +1,10 @@
 require 'fileutils'
-FileUtils.rm_r(File.join(File.dirname(__FILE__), '../../../public/images/plupload-rails3'))
-FileUtils.rm_r(File.join(File.dirname(__FILE__), '../../../public/javascripts/plupload-rails3'))
-FileUtils.rm_r(File.join(File.dirname(__FILE__), '../../../public/stylesheets/plupload-rails3'))
+
+image_path_prefix = '../../../public'
+js_path_prefix = '../../../public'
+css_path_prefix = '../../../public'
+
+FileUtils.rm_r(File.join(File.dirname(__FILE__), "#{image_path_prefix}/images/plupload-rails3"))
+FileUtils.rm_r(File.join(File.dirname(__FILE__), "#{js_path_prefix}/public/javascripts/plupload-rails3"))
+FileUtils.rm_r(File.join(File.dirname(__FILE__), "#{css_path_prefix}/public/stylesheets/plupload-rails3"))
 FileUtils.rm_r(File.join(File.dirname(__FILE__),'../../../tmp/plupload-rails3'))
