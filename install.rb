854043200d52eb3ac53efb728b93ead15af29635
Sammy Larbi <sam@codeodor.com>
2011-10-12 11:55:53 -0500

    change require path for install and uninstall script to hopefully fix it

--- install.rb
+++ install.rb
@@ -1,2 +1,2 @@
-require File.join(File.dirname(__FILE__), 'lib/plupload-rails3/asset_mover')
+require File.join(Rails.root, File.dirname(__FILE__), 'lib/plupload-rails3/asset_mover')
 install_plupload_assets
