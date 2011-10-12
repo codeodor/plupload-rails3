af879b4be66c62161f84ef347bc17080a05d1f14
Sammy Larbi <sam@codeodor.com>
2011-10-12 12:06:21 -0500

    change require path for install and uninstall script to hopefully fix it

--- install.rb
+++ install.rb
@@ -1,7 +1,9 @@
-begin 
-  require File.join(File.dirname(__FILE__), 'lib/plupload-rails3/asset_mover')
-rescue 
-  #to install locally, I have to use rails runner instead of rails plugin install, and it gives a messed up path
+asset_mover_file = File.join(File.dirname(__FILE__), 'lib/plupload-rails3/asset_mover')
+
+if File.exists?(asset_mover_file) 
+  require asset_mover_file
+else #to install locally, I have to use rails runner instead of rails plugin install, and it gives a messed up path
   require File.join(Rails.root, File.dirname(__FILE__), 'lib/plupload-rails3/asset_mover')
 end
+
 install_plupload_assets
